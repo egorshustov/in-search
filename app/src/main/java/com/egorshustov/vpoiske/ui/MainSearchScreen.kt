a54858a008e6b6752fdd2c6cbfad011904542627
Egor Shustov <shustov_egor@mail.ru>
2021-12-22 15:44:06 +0300

    MainSearchScreenDrawer created, drawerItems added

--- app/src/main/java/com/egorshustov/vpoiske/ui/MainSearchScreen.kt
+++ app/src/main/java/com/egorshustov/vpoiske/ui/MainSearchScreen.kt
@@ -1,29 +1,51 @@
 package com.egorshustov.vpoiske.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.egorshustov.vpoiske.ui.theme.VPoiskeTheme
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.egorshustov.vpoiske.R
 
 @Composable
 fun MainSearchScreen() {
-    // A surface container using the 'background' color from the theme
-    Surface(color = MaterialTheme.colors.background) {
-        Greeting("Android")
+    val scaffoldState = rememberScaffoldState()
+    Scaffold(
+        scaffoldState = scaffoldState,
+        drawerContent = {
+            MainSearchScreenDrawer()
+        }
+    ) {
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+private val drawerItems = listOf(
+    "Последний поиск",
+    "Новый поиск",
+    "История поисков",
+    "Сменить тему"
+)
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    VPoiskeTheme {
-        Greeting("Android")
+fun MainSearchScreenDrawer(modifier: Modifier = Modifier) {
+    Column(
+        modifier
+            .fillMaxSize()
+            .padding(start = 24.dp, top = 48.dp)
+    ) {
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_foreground),
+            contentDescription = stringResource(R.string.open_drawer)
+        )
+        for (screen in drawerItems) {
+            Spacer(Modifier.height(24.dp))
+            Text(text = screen, style = MaterialTheme.typography.h4)
+        }
     }
 }
