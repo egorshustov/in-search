56a5db4575ed17998cb80f96c59460ed3500ae55
Egor Shustov <shustov_egor@mail.ru>
2022-01-08 11:30:49 +0300

    Added: Firebase Crashlytics, Timber, Result generic class, UseCases coroutines base classes

--- app/src/main/java/com/egorshustov/vpoiske/VPoiskeApplication.kt
+++ app/src/main/java/com/egorshustov/vpoiske/VPoiskeApplication.kt
@@ -1,7 +1,19 @@
 package com.egorshustov.vpoiske
 
 import android.app.Application
+import com.egorshustov.vpoiske.utils.CrashlyticsTree
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class VPoiskeApplication : Application()
+class VPoiskeApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        } else {
+            Timber.plant(CrashlyticsTree())
+        }
+    }
+}
---
+++ core/common/src/main/java/com/egorshustov/core/common/model/Result.kt
@@ -0,0 +1,39 @@
+package com.egorshustov.core.common.model
+
+import com.egorshustov.core.common.model.Result.Success
+import kotlinx.coroutines.flow.MutableStateFlow
+
+/**
+ * A generic class that holds a value with its loading status.
+ * @param <T>
+ */
+sealed class Result<out R> {
+
+    data class Success<out T>(val data: T) : Result<T>()
+    data class Error(val exception: Exception) : Result<Nothing>()
+    object Loading : Result<Nothing>()
+
+    override fun toString(): String = when (this) {
+        is Success<*> -> "Success[data=$data]"
+        is Error -> "Error[exception=$exception]"
+        Loading -> "Loading"
+    }
+}
+
+/**
+ * `true` if [Result] is of type [Success] & holds non-null [Success.data].
+ */
+val Result<*>.succeeded
+    get() = this is Success && data != null
+
+fun <T> Result<T>.successOr(fallback: T): T = (this as? Success<T>)?.data ?: fallback
+
+val <T> Result<T>.data: T?
+    get() = (this as? Success)?.data
+
+/**
+ * Updates value of [MutableStateFlow] if [Result] is of type [Success]
+ */
+inline fun <reified T> Result<T>.updateOnSuccess(stateFlow: MutableStateFlow<T>) {
+    if (this is Success) stateFlow.value = data
+}
