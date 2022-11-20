7ccdbc50a87b0d2c35dd7d41d63e5219405176de
Egor Shustov <shustov_egor@mail.ru>
2022-11-20 19:17:20 +0500

    App colors implemented

--- core/ui/src/main/java/com/egorshustov/insearch/core/ui/theme/Theme.kt
+++ core/ui/src/main/java/com/egorshustov/insearch/core/ui/theme/Theme.kt
@@ -5,16 +5,59 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
-private val DarkColorScheme = darkColorScheme(
-    primary = ColorNightRider,
-    secondary = ColorDenim
+val LightColorScheme = lightColorScheme(
+    primary = Purple40,
+    onPrimary = Color.White,
+    primaryContainer = Purple90,
+    onPrimaryContainer = Purple10,
+    secondary = Orange40,
+    onSecondary = Color.White,
+    secondaryContainer = Orange90,
+    onSecondaryContainer = Orange10,
+    tertiary = Blue40,
+    onTertiary = Color.White,
+    tertiaryContainer = Blue90,
+    onTertiaryContainer = Blue10,
+    error = Red40,
+    onError = Color.White,
+    errorContainer = Red90,
+    onErrorContainer = Red10,
+    background = DarkPurpleGray99,
+    onBackground = DarkPurpleGray10,
+    surface = DarkPurpleGray99,
+    onSurface = DarkPurpleGray10,
+    surfaceVariant = PurpleGray90,
+    onSurfaceVariant = PurpleGray30,
+    outline = PurpleGray50
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = ColorSolitude,
-    secondary = ColorTropicalBlue
+val DarkColorScheme = darkColorScheme(
+    primary = Purple80,
+    onPrimary = Purple20,
+    primaryContainer = Purple30,
+    onPrimaryContainer = Purple90,
+    secondary = Orange80,
+    onSecondary = Orange20,
+    secondaryContainer = Orange30,
+    onSecondaryContainer = Orange90,
+    tertiary = Blue80,
+    onTertiary = Blue20,
+    tertiaryContainer = Blue30,
+    onTertiaryContainer = Blue90,
+    error = Red80,
+    onError = Red20,
+    errorContainer = Red30,
+    onErrorContainer = Red90,
+    background = DarkPurpleGray10,
+    onBackground = DarkPurpleGray90,
+    surface = DarkPurpleGray10,
+    onSurface = DarkPurpleGray90,
+    surfaceVariant = PurpleGray30,
+    onSurfaceVariant = PurpleGray80,
+    outline = PurpleGray60
 )
 
 @Composable
@@ -22,7 +65,7 @@ fun InSearchTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composab
     val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
 
     val systemUiController = rememberSystemUiController()
-    systemUiController.setSystemBarsColor(if (darkTheme) ColorNightRider else ColorSolitude)
+    systemUiController.setSystemBarsColor(if (darkTheme) DarkPurpleGray10 else DarkPurpleGray99)
 
     MaterialTheme(
         colorScheme = colorScheme,
